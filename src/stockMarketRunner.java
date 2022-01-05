86dcf6c6972837753504ec4fe30b398e7e8ec63f
Ann A <31941055+an-rainbow@users.noreply.github.com>
2022-01-04 18:32:32 -0800

    Create stockMarketRunner.java

---
+++ src/stockMarketRunner.java
@@ -0,0 +1,14 @@
+public class StockMarketRunner {
+
+    public StockMarketRunner() {
+        
+    }
+    /**
+     * Runs the Stock Market program
+     * @param args
+     */
+    public static void main(String[] args) {
+        ProgramDashboard launch = new ProgramDashboard();
+        launch.launchProgram();
+    }
+}
