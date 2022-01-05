0b7d3e70a1df9205a185c77809048aa227740520
Ann A <31941055+an-rainbow@users.noreply.github.com>
2022-01-04 18:31:03 -0800

    Create stockdata.java

---
+++ src/stockdata.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class StockData {
+    boolean debug = true;
+
+    ArrayList<String[]> input = new ArrayList<String[]>();
+
+    private void log(String s) {
+        if (debug) {
+          System.out.print(s);
+        }      
+    }  
+       
+    private boolean readData(String filename) {
+        File f=new File(filename);
+               
+        int ct = 0;
+        try {  
+          Scanner fileReader=new Scanner(f);
+          while(fileReader.hasNextLine()) {
+            String line = fileReader.nextLine();
+            String[] values = line.split(",");
+            input.add(values);
+                               
+            log(ct + ": ");
+            if (ct == 0) {  // Get field names.
+              for (String v : values) { log(v + " "); }
+              log("\n");
+            } else {
+              for (String v : values) { log(v); }
+              log("\n");
+            }          
+                               
+            ct += 1;    
+          }      
+          fileReader.close();
+                               
+          log("Number of records read: " + ct + "\n");
+          log("Input size: " + input.size() + "\n");
+
+
+                 
+          return true;
+        } catch (FileNotFoundException e) {
+          System.out.println("Error: " + e);
+          return false;
+        }      
+       
+    }  
+       
+    public static void main(String[] args) {
+        StockData m = new StockData();
+        m.readData("/Users/anna/Desktop/AAPL.csv");
+    }  
+
+}
